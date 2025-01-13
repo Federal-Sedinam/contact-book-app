c80b908c7c910f245eaa2e4c6a8289958796abfd
Federal Sedinam <federal@Federals-Laptop.local>
2025-01-13 12:36:58 +0000

    Add quit and theme toggle buttons

--- contactbook/tui.py
+++ contactbook/tui.py
@@ -1,7 +1,15 @@
 from textual.app import App
-from textual.widgets import Footer, Header
+from textual.containers import Grid
+from textual.screen import Screen
+from textual.widgets import Button, Footer, Header, Label
 
 class ContactsApp(App):
+    CSS_PATH = "contactbook.tcss"
+    BINDINGS = [
+        ("m", "toggle_dark", "Toggle dark mode"),
+        ("q", "request_quit", "Quit"),
+    ]
+
     def compose(sef):
         yield Header()
         yield Footer()
@@ -9,4 +17,35 @@ class ContactsApp(App):
     def on_mount(self):
         self.title = "RP Contacts"
         self.sub_title = "A Contacts Book App With Textual And Python"
+
+    def action_toggle_dark(self):
+        return super().action_toggle_dark()
+    
+    def action_request_quit(self):
+        def check_answer(accepted):
+            if accepted:
+                self.exit()
         
+        self.push_screen(QuestionDialog("Do you want to quit?"), check_answer)
+    
+class QuestionDialog(Screen):
+    def __init__(self, message, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.message = message
+
+    def compose(self):
+        no_button = Button("No", variant="primary", id="no")
+        no_button.focus()
+
+        yield Grid(
+            Label(self.message, id="question"),
+            Button("Yes", variant="error", id="yes"),
+            no_button,
+            id="question-dialog",
+        )
+
+    def on_button_pressed(self, event):
+        if event.button.id == "yes":
+            self.dismiss(True)
+        else:
+            self.dismiss(False)
