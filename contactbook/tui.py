9a962d44ae6a5dab75ceb4501a958e0bc6cb0cf1
Federal Sedinam <federal@Federals-Laptop.local>
2025-01-14 14:15:05 +0000

    Built TUI

--- contactbook/tui.py
+++ contactbook/tui.py
@@ -1,17 +1,35 @@
 from textual.app import App
-from textual.containers import Grid
+from textual.containers import Grid, Horizontal, Vertical
 from textual.screen import Screen
-from textual.widgets import Button, Footer, Header, Label
+from textual.widgets import Button, Footer, Header, Label, DataTable, Static
 
 class ContactsApp(App):
     CSS_PATH = "contactbook.tcss"
     BINDINGS = [
         ("m", "toggle_dark", "Toggle dark mode"),
+        ("a", "add", "Add"),
+        ("d", "delete", "Delete"),
+        ("c", "clear_all", "Clear All"),
         ("q", "request_quit", "Quit"),
     ]
 
-    def compose(sef):
+    def compose(self):
         yield Header()
+        contacts_list = DataTable(classes="contacts-list")
+        contacts_list.focus()
+        contacts_list.add_columns("Name", "Phone", "Email")
+        contacts_list.cursor_type = "row"
+        contacts_list.zebra_stripes = True
+        add_button = Button("Add", variant="success", id="add")
+        add_button.focus()
+        buttons_panel = Vertical(
+            add_button,
+            Button("Delete", variant="warning", id="delete"),
+            Static(classes="separator"),
+            Button("Clear All", variant="error", id="clear"),
+            classes="buttons-panel",
+        )
+        yield Horizontal(contacts_list, buttons_panel)
         yield Footer()
 
     def on_mount(self):
